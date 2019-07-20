f48364da66846de7cea9cc5e1806020982313bc1
scootyboots <scott.coffrin@gmail.com>
2019-07-20 20:13:21 +0900

    choses img randomly on load

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const marthImgs = [
@@ -9,12 +8,19 @@ const marthImgs = [
   "https://png2.kisspng.com/sh/a556e3279d5047c7e87626a8d9ca1bf0/L0KzQYm3VsMxN6pvjZH0aYP2gLBuTgN2eJZ3ReV2YYPrPbP5jCMudZZxfdc2cHnucbTvlb1uaaN5gJ9saHH1cbT7hgIubpoyhdN7dHiwRbO6V8Q5aZM9TtRvZkmxRIK3Ucg3O2o2TaU5M0S8R4SCVcUyQV91htk=/kisspng-super-smash-bros-melee-pikachu-marth-character-fi-marth-5b3748ab86bff9.4101863915303497395519.png"
 ]
 
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const chosenImg = getRandomInt(marthImgs.length);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Marth me up baby</p>
-        <img src={marthImgs[2]} width="600px"/>
+        <img src={marthImgs[chosenImg]} width="600px" alt="marth image"/>
         <p>get better or bend over</p>
       </header>
     </div>
